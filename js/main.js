3a68cdbb9a61413c2ef8602607dc8a4c342fc559
JuniorFirster <zhangxing114@126.com>
2017-08-27 18:12:06 +0800

    添加一些libs文件，修改一些文件地址

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 /**
  * Created by Administrator on 2017/8/27 0027.
  */
+require.config({
+    baseUrl: "./js",
+    path:{
+        "require": "../libs/require",
+        "jquery": "../libs/jquery.min"
+    }
+})
