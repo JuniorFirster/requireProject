36fc5f924da8878a5e45966511c36fa4984ac97c
JuniorFirster <zhangxing114@126.com>
2017-08-30 21:46:33 +0800

    架构主框架

---
+++ js/config/filter.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/8/30 0030.
+ */
+define([
+    "require",
+    "jquery"
+],function(require,$){
+
+    var FilterObject = {
+        //内容略……
+    }
+
+    return FilterObject;
+
+})
--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 /**
  * Created by Administrator on 2017/8/27 0027.
  */
+define(
+    [
+        "require",
+        "jquery",
+        "config/global",
+        "config/filter",
+        "config/route",
+    ],
+    function (require, $) {
+        var IndexOperation = {
+            //内容略……
+
+        }
+
+        return IndexOperation
+    }
+)
